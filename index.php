aa654ef7ae09ce202c0a76a4254684a56c3e0e7a
erkim97 <kimeric97@hotmail.com>
2021-04-11 23:46:25 -0700

    test php wa

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include_once("./frontend/"); ?>
+<?php
+include_once("./frontend/login/login.html");
+include_once("./frontend/register/register.html");
+include_once("./frontend/index/index.html");
+include_once("./frontend/admin/admin.html");
+include_once("./frontend/documentation/documentation.html");
+include_once("./frontend/assets/");
+?>
