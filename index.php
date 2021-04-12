1132a6bb0c04b81279a194fe256439ebeec73e5c
erkim97 <kimeric97@hotmail.com>
2021-04-11 23:57:27 -0700

    Refactoring for index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-include_once("./frontend/login/login.html");
-include_once("./frontend/register/register.html");
-include_once("./frontend/index/index.html");
-include_once("./frontend/admin/admin.html");
-include_once("./frontend/documentation/documentation.html");
-include_once("./frontend/assets/");
-?>
+include_once("./frontend/login.html");
+include_once("./frontend/registration.html");
+include_once("./frontend/index.html");
+>
